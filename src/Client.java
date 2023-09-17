c59aba50f42cc53b474696b49b92f881e8fbfbdb
Reese Hatfield <hatfield.69@wright.edu>
2023-09-17 13:57:44 -0400

    Create Server selection Screen

--- src/Client.java
+++ src/Client.java
@@ -14,8 +14,9 @@ import java.util.UUID;
 
 public class Client {
     public static void main(String[] args) {
-        new Client();
+        new ServerSelect();
     }
+
     private static String lastData = "";
     private static int xCoord = 0;
     private static int yCoord = 0;
@@ -27,9 +28,9 @@ public class Client {
     // so it is still unique per client
     private final static String uid = UUID.randomUUID().toString();
 
-    public Client() {
+    public Client(String ip) {
         Scanner scanner = new Scanner(System.in);
-
+        this.serverIP = ip;
 
         Backend backend = new Backend(scanner);
         // create GUI here
@@ -37,24 +38,25 @@ public class Client {
 
         for (Ship s : backend.getShips()) {
             for (Point p : s.points) {
-                HittableButton button = board.playerPanelList.get(p.y*10+p.x);
-                board.setHealth(board.getHealth() +1);
+                HittableButton button = board.playerPanelList.get(p.y * 10 + p.x);
+                board.setHealth(board.getHealth() + 1);
                 button.setBackground(Color.GREEN);
                 button.setShipStatus(true);
 
             }
         }
 
-        System.out.println("Enter the server IP: ");
-        scanner.nextLine();
-        serverIP = scanner.nextLine();
+        // System.out.println("Enter the server IP: ");
+        // scanner.nextLine();
+        // serverIP = scanner.nextLine();
         Timer timer = new Timer();
         timer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
                 try {
                     // GET request to fetch data
-                    URL getUrl = new URL("http://" + serverIP + ":" + Settings.PORT_NUMBER + Settings.SERVER_ENDPOINT + "?" + uid);
+                    URL getUrl = new URL(
+                            "http://" + serverIP + ":" + Settings.PORT_NUMBER + Settings.SERVER_ENDPOINT + "?" + uid);
                     HttpURLConnection getCon = (HttpURLConnection) getUrl.openConnection();
                     getCon.setRequestMethod("GET");
 
@@ -67,34 +69,33 @@ public class Client {
                     }
                     in.close();
 
-                    //Update board here
-                    //check other strings for game setup so two players connect
+                    // Update board here
+                    // check other strings for game setup so two players connect
                     if (!response.toString().equals("No data from other client")) {
                         System.out.println(response.toString());
 
-                        //Expected string UUID;x,y,didHit,didWin
+                        // Expected string UUID;x,y,didHit,didWin
                         String[] parts = response.toString().split(",");
                         int responseX = Integer.parseInt(parts[0]);
                         int responseY = Integer.parseInt(parts[1]);
 
-                        //Win before more shooting
+                        // Win before more shooting
                         if (Integer.parseInt(parts[3]) == 1) {
                             board.winningMenu();
                         }
 
-                        //No win, so check if hit
-                        //if hit, send back hit without firing request
-                        if (Integer.parseInt(parts[2]) == 1) { //Change this to check hit status
-                            //POST with hit
-                            //if xCoord || yCoord > 9 do not fire anything
+                        // No win, so check if hit
+                        // if hit, send back hit without firing request
+                        if (Integer.parseInt(parts[2]) == 1) { // Change this to check hit status
+                            // POST with hit
+                            // if xCoord || yCoord > 9 do not fire anything
                             String postData = uid + ";" + "10" + "," + ",10" + ",1" + ",0";
                         }
 
-
                         if (xCoord < 9 && yCoord < 9) {
-                            HittableButton button = board.playerPanelList.get(responseY*10+responseX);
+                            HittableButton button = board.playerPanelList.get(responseY * 10 + responseX);
                             if (button.hit()) {
-                                board.setHealth(board.getHealth()-1);
+                                board.setHealth(board.getHealth() - 1);
                             }
                         }
                     }
@@ -104,53 +105,51 @@ public class Client {
                         lastData = response.toString();
                         System.out.println("New data received: " + response);
 
-                        while(pause) {
+                        while (pause) {
                         }
                         pause = true;
 
                         boolean validInput = false;
-                        while(!validInput){
-                             /*System.out.println("Enter your new X coordinate: ");
-                             xCoord = scanner.nextInt();
-                             System.out.println("Enter your new Y coordinate: ");
-                             yCoord = scanner.nextInt();*/
+                        while (!validInput) {
+                            /*
+                             * System.out.println("Enter your new X coordinate: ");
+                             * xCoord = scanner.nextInt();
+                             * System.out.println("Enter your new Y coordinate: ");
+                             * yCoord = scanner.nextInt();
+                             */
 
-                             validInput = ((0 <= xCoord && xCoord <= 9) && (0 <= yCoord && yCoord <= 9));
+                            validInput = ((0 <= xCoord && xCoord <= 9) && (0 <= yCoord && yCoord <= 9));
 
-                             if(!validInput) {
-                                 System.out.println("Invalid input, must be between 0 and 9");
-                             }
+                            if (!validInput) {
+                                System.out.println("Invalid input, must be between 0 and 9");
+                            }
 
                         }
                         // Read new coordinates from user
 
-
-                        board.oPanelList.get(yCoord*10+xCoord).setBackground(Color.BLACK);
+                        board.oPanelList.get(yCoord * 10 + xCoord).setBackground(Color.BLACK);
                         postData(xCoord, yCoord, board);
+                    } else {
+                        postData(0, 0, board);
                     }
-                    else {
-                        postData(0,0,board);
-                    }
-                }
-                catch (Exception e) {
+                } catch (Exception e) {
                     ErrorHandler.handleError(e);
                 }
             }
-        }, 0, 1000);  // Check every second
+        }, 0, 1000); // Check every second
     }
 
-    public void postData(int xCoord, int yCoord, Board board){
+    public void postData(int xCoord, int yCoord, Board board) {
 
         try {
             pause = false;
 
-
-        /*
-         POST new data
-         Format for data:
-         "UID;x,y,0"
-         That zero is reserved for now, will be used to check win condition
-        */
+            /*
+             * POST new data
+             * Format for data:
+             * "UID;x,y,0"
+             * That zero is reserved for now, will be used to check win condition
+             */
             String postData = this.uid + ";" + xCoord + "," + yCoord + ",0";
             if (board.getHealth() == 0) {
 
@@ -171,8 +170,7 @@ public class Client {
             wr.close();
 
             postCon.getResponseCode();
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             ErrorHandler.handleError(e);
         }
     }
